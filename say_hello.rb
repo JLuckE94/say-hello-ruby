d47bf4cf0eaa67f94371cb659ae787d67637ee41
Joseph <JyeonO94@Gmail.com>
2020-12-08 18:07:03 -0500

    commit tree f7c16bc931bb805617e57aa46c5b8ed4985cff58

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
 # Build your say_hello method here
+
+def say_hello
+    puts "Hello, Ruby programmer!"
+end 
+#             assigning a default value
+def say_hello(name, language = "Ruby programmer")
+  puts "Hello, #{name}"
+end 
+say_hello
